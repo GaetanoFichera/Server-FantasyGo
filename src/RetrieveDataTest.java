e2c2784aba2d1cb21d74afd6b4eb93dda9b9e2e6
Gaetano Fichera <gaetano.fichera93@gmail.com>
2017-10-11 16:18:19 +0000

    .

---
+++ src/RetrieveDataTest.java
@@ -0,0 +1,49 @@
+import Entity.ZonaDiCaccia;
+import Utils.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
+
+import java.util.List;
+
+/**
+ * Created by gaetano on 08/10/17.
+ */
+public class RetrieveDataTest {
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+        Session session = sessionFactory.openSession();
+
+        try {
+            List<ZonaDiCaccia> zonedicaccias = session.createQuery(
+                    "select z " +
+                            "from " + ZonaDiCaccia.class.getName() + " z ")
+                    .list();
+
+            for (ZonaDiCaccia zone : zonedicaccias) {
+                System.out.println("ZoneDiCaccia: " + zone.getId() + " : "
+                        + zone.getCentroLatitudine() + " : "
+                        + zone.getCentroLongitudine() + " : "
+                        + zone.getCoordinataCentro().getLatitudine() + " : "
+                        + zone.getCoordinataCentro().getLongitudine());
+            }
+
+            // Commit data.
+            if (session.getTransaction().getStatus().equals(TransactionStatus.ACTIVE)) {
+                session.getTransaction().commit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Rollback in case of an error occurred.
+            session.getTransaction().rollback();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+
+        HibernateUtil.shutdown();
+    }
+}
